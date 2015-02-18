17a4c7ccc1bbe9c8e41733e4dad6a7a4f18fbd55
toshko93 <toshco_93@abv.bg>
2015-02-18 19:21:11 +0200

    Added Enemy.cs

---
+++ OOP Teamwork v1.0/The Powerful Game/Entities/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Powerful_Game.Entities
+{
+    class Enemy : Entity
+    {
+        public Enemy(string name, int hitPts, int armor, int entityDamage, double attackSpeed) 
+            : base(name, hitPts, armor, entityDamage, attackSpeed)
+        {
+        }
+    }
+}
