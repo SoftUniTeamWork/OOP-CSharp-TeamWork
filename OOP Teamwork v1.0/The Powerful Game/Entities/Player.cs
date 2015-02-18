e6ba71f4e1a8e00ab5cf76c1ad436520badc3167
toshko93 <toshco_93@abv.bg>
2015-02-18 16:25:13 +0200

    Added Player class.

---
+++ OOP Teamwork v1.0/The Powerful Game/Entities/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+using Entities.Chooses;
+
+namespace The_Powerful_Game.Entities.Chooses
+{
+    class Player : Entity
+    {
+        private int resoursePoints;
+        private EntityResourceType resourceType;
+
+        public Player(string name, int hitPts, int armor, int entityDamage, double attackSpeed, int resoursePts, EntityResourceType resourceType)
+            : base(name, hitPts, armor, entityDamage, attackSpeed)
+        {
+            this.ResoursePoints = resoursePts;
+            this.ResourceType = resourceType;
+        }
+        public int ResoursePoints { get; set; }
+        public EntityResourceType ResourceType { get; set; }
+    }
+}
