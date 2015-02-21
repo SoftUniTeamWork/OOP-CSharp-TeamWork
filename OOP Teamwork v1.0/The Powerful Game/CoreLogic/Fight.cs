1045532d0182572fe458469720c7cca107da2205
toshko93 <toshco_93@abv.bg>
2015-02-21 21:34:19 +0200

    Fixed Fight

--- OOP Teamwork v1.0/The Powerful Game/CoreLogic/Fight.cs
+++ OOP Teamwork v1.0/The Powerful Game/CoreLogic/Fight.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using The_Powerful_Game.Entities;
 
 namespace The_Powerful_Game.CoreLogic
@@ -11,7 +12,7 @@ namespace The_Powerful_Game.CoreLogic
     {
         private Player player;
         private Enemy enemy;
-        private bool fightOver = false;
+        private bool fightOver;
 
         public Fight(Player player, Enemy enemy)
         {
@@ -21,7 +22,7 @@ namespace The_Powerful_Game.CoreLogic
 
         public Player Player { get; set; }
         public Enemy Enemy { get; set; }
-
+        public bool FightOver { get; set; }
         public void EnemyTurn()
         {
             Random fightSituation = new Random();
@@ -33,7 +34,7 @@ namespace The_Powerful_Game.CoreLogic
                 case 1:
                 case 2:
                     // Enemy deals 100% damage
-                    Player.HealthPoints = Player.HealthPoints - ((int)damage - Player.ArmorPoints);
+                    Player.HealthPoints = Player.HealthPoints - (damage - Player.ArmorPoints);
                     result = "The Enemy hits you, dealing " + damage + " damage.";
                     break;
                 case 3:
@@ -73,6 +74,72 @@ namespace The_Powerful_Game.CoreLogic
                     break;
             }
 
+            // Result for message box
+            FightOverCheck();
+        }
+
+        public void PlayerTurn(string userChoice)
+        {
+            Random fightSituation = new Random();
+            string result = "";
+            int damage = Player.Damage;
+
+            if (userChoice == "Attack")
+            {
+                int fightCase = fightSituation.Next(1, 10);
+                switch (fightCase)
+                {
+                    case 1:
+                    case 2:
+                        // Deal 100% damage
+                        Enemy.HealthPoints = Enemy.HealthPoints - (damage - Enemy.ArmorPoints);
+                        result = "You deal " + damage + " damage.";
+                        break;
+                    case 3:
+                    case 4:
+                        // Deal 120% damage
+                        damage = (int)Math.Round(damage * 6 / 5.0); // 120% dmg
+                        Enemy.HealthPoints = Enemy.HealthPoints - (damage - Enemy.ArmorPoints);
+                        result = "You strike for " + damage + " damage.";
+                        break;
+                    case 5:
+                    case 6:
+                        // Deal 80% damage
+                        damage = (int)Math.Round(damage * 4 / 5.0); // 80% dmg
+                        Enemy.HealthPoints = Enemy.HealthPoints - (damage - Enemy.ArmorPoints);
+                        result = "You hit for " + damage + " damage.";
+                        break;
+                    case 7:
+                        // Deal Critical 200% damage
+                        damage = damage * 2; // 200% dmg
+                        Enemy.HealthPoints = Enemy.HealthPoints - (damage - Enemy.ArmorPoints);
+                        result = "You attack with a massive blow for " + damage + " damage.";
+                        break;
+                    case 8:
+                        // Miss
+                        result = "You miss your enemy.";
+                        break;
+                    case 9:
+                        // Stun for 1 turn and 50% damage
+                        damage = damage / 2; // 50% dmg
+                        Enemy.HealthPoints = Enemy.HealthPoints - (damage - Enemy.ArmorPoints);
+                        result = "With a fierce strike you deal " + damage + " damage and stun your opponent for 1 round.";
+                        break;
+                    case 10:
+                        // 
+                        break;
+                }
+            }
+            else if (userChoice == "Spell 1")
+            {
+                
+            }
+            else if (userChoice == "Spell 2")
+            {
+                
+            }
+
+            // Result for message box
             FightOverCheck();
         }
 
@@ -88,7 +155,7 @@ namespace The_Powerful_Game.CoreLogic
                 {
                     // You killed your enemy gain xp and reward
                 }
-                fightOver = true;
+                FightOver = true;
             }
         }
     }
