ff2067144478b8a0263aa33cbd279efafd3cb88a
SStoyanov22 <SStoyanov22@gmail.com>
2015-02-23 20:38:16 +0200

    Adding new window state  + AttributePair class

---
+++ OOP Teamwork v1.0/The Powerful Game/Entities/AttributePair.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Powerful_Game.Entities
+{
+    public struct AttributePair
+    {
+        //Fields
+        private int currentValue;
+        private int maximumValue;
+
+        //Constructors
+        /// <summary>
+        /// This constructor set attribute max value.
+        /// </summary>
+        /// <param name="maxValue">the max value</param>
+        public AttributePair(ushort maxValue)
+        {
+            this.currentValue = maxValue;
+            this.maximumValue = maxValue;
+        }
+
+        //Properties
+        public int CurrentValue
+        {
+            get
+            {
+                return currentValue;
+            }
+        }
+
+        public int MaximumValue
+        {
+            get
+            {
+                return maximumValue;
+            }
+        }
+
+        public static AttributePair Zero
+        {
+            get
+            {
+                return new AttributePair();
+            }
+        }
+
+        
+
+        //Methods
+        /// <summary>
+        /// This method increment heal value and checks value for overflow.
+        /// </summary>
+        /// <param name="value">add health</param>
+        public void Heal(ushort value)   // value is ushort type, so that negative values can't be passed
+        {
+            currentValue += value;
+            if (currentValue > maximumValue)
+            {
+                currentValue = maximumValue;
+            }
+        }
+
+        /// <summary>
+        /// This method takes damage. Checks if damage is zero.
+        /// </summary>
+        /// <param name="value">take damage</param>
+        public void Damage(ushort value)
+        {
+            currentValue -= value;
+            if (currentValue < 0)
+            {
+                currentValue = 0;
+            }
+        }
+
+        /// <summary>
+        /// This method sets the current value.
+        /// </summary>
+        /// <param name="value">current value</param>
+        public void SetCurrent(ushort value)  // used to set the currentValue
+        {
+            currentValue = value;
+            if (currentValue > maximumValue)
+            {
+                currentValue = maximumValue;
+            }
+        }
+
+        /// <summary>
+        /// This method sets the max value
+        /// </summary>
+        /// <param name="value">max value</param>
+        public void SetMaximum(ushort value) // used to set the maximumValue
+        {
+            maximumValue = value;
+            if (currentValue > maximumValue)
+            {
+                currentValue = maximumValue;
+            }
+        }
+    }
+}
---
+++ OOP Teamwork v1.0/The Powerful Game/Menu/Choosing.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace The_Powerful_Game.Menu
+{
+    /// <summary>
+    /// Interaction logic for Choosing.xaml
+    /// </summary>
+    public partial class Choosing : UserControl, ISwitchable
+    {
+        public Choosing()
+        {
+            InitializeComponent();
+        }
+
+        private void warriorButton_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void mageButton_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        public void UtilizeState(object state)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void hunterButton_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
