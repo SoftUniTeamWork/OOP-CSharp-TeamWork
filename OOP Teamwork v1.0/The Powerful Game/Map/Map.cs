0363a38471c3afbed5b1c0fbf1c01c7879b893a8
SStoyanov22 <SStoyanov22@gmail.com>
2015-02-25 20:48:44 +0200

    Added Map folder + Map class and Tile Class

---
+++ OOP Teamwork v1.0/The Powerful Game/Map/Map.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace The_Powerful_Game.Map
+{
+    class Map
+    {
+        //Fields
+        private int rows;
+        private int columns;
+        private List<Tile> tiles = new List<Tile>(); 
+        //Constructors
+        public Map(int rows, int columns)
+        {
+            this.Rows = rows;
+            this.Columns = columns;
+            Random random = new Random();
+            for (int r = 0; r < columns; r++)
+            {
+                for (int c = 0; c < columns; c++)
+                {
+                    var tile = new Tile();
+                    tile.Data = string.Format("Row {0}, Column {1}", r, c);
+                    tile.Bacground =
+                        new SolidColorBrush(Color.FromArgb(255, (byte) random.Next(256), (byte) random.Next(256),
+                            (byte) random.Next(256)));
+                    tiles.Add(tile);
+
+                }
+            }
+        }
+        //Properties
+
+        public int Rows
+        {
+            get { return this.rows; }
+            set { this.rows = value; }
+        }
+        public int Columns
+        {
+            get { return this.columns; }
+            set { this.columns = value; }
+        }
+
+        public List<Tile> Tiles
+        {
+            get { return this.tiles; }
+            set { this.tiles = value; }
+        }
+    }
+}
---
+++ OOP Teamwork v1.0/The Powerful Game/Map/Tile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media;
+
+namespace The_Powerful_Game.Map
+{
+    public class Tile
+    {
+        public string Data { get; set; }
+        public SolidColorBrush Background { get; set; }
+    }
+}
