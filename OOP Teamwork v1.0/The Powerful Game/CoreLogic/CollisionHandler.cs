53aa96989133745e2ee7ce4e25f55a8a5d9d660f
OmniPot <r.conkov@gmail.com>
2015-02-26 20:11:52 +0200

    Collision Debugging
    Collision Debugging

--- OOP Teamwork v1.0/The Powerful Game/CoreLogic/CollisionHandler.cs
+++ OOP Teamwork v1.0/The Powerful Game/CoreLogic/CollisionHandler.cs
@@ -1,4 +1,7 @@
-﻿namespace The_Powerful_Game.CoreLogic
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace The_Powerful_Game.CoreLogic
 {
     using System;
     using System.Windows.Media;
@@ -17,11 +20,29 @@
 
             double distance = Math.Sqrt(Math.Pow(playerX - enemyX, 2) + Math.Pow(playerY - enemyY, 2));
 
-            if (distance <= player.Image.Width / 2 &&
-                distance <= enemy.Image.Height / 2)
+            if (distance <= player.Image.Width * 2 / 3 &&
+                distance <= enemy.Image.Height * 3 / 2)
             {
                 CompositionTarget.Rendering -= Gameplay.MainEngine.Run;
                 Switcher.Switch(new FightField(player, enemy));
+            }
+        }
+
+        public static void HandleMapObjectCollision(Character player)
+        {
+            int x = (int)(player.X + player.Image.Width / 2) / 25;
+            int y = (int)(player.Y + player.Image.Height / 2) / 25;
+
+            if (Keyboard.IsKeyDown(Key.Enter))
+            {
+                if (Gameplay.numMap[y][x] == 0)
+                {
+                    MessageBox.Show(x.ToString() + "\n" + y.ToString() + "\nGRASS");
+                }
+                if (Gameplay.numMap[y][x] == 1)
+                {
+                    MessageBox.Show(x.ToString() + "\n" + y.ToString() + "\nFOREST");
+                }
             }
         }
     }
--- OOP Teamwork v1.0/The Powerful Game/Entities/Character.cs
+++ OOP Teamwork v1.0/The Powerful Game/Entities/Character.cs
@@ -89,6 +89,7 @@
             {
                 this.isAlive = false;
             }
+            CollisionHandler.HandleMapObjectCollision(this);
             Move();
         }
 
